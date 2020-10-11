f64a681ed53cf030b060d57567831ae522f48bd8
wjdxor133 <mahan89@daum.net>
2020-10-11 23:25:32 +0900

    리덕스 Hooks 적용

--- src/containers/CounterContainer.js
+++ src/containers/CounterContainer.js
@@ -1,80 +1,26 @@
-import React from "react";
-import { connect } from "react-redux";
-// import { bindActionCreators } from "redux";
+import React, { useCallback } from "react";
+//connect 함수를 사용하는 대신 react-redux에서 제공하는 Hooks를 사용할 수도 있다.
+import { useSelector, useDispatch } from "react-redux";
 import Counter from "../components/Counter";
 import { increase, decrease } from "../modules/counter";
 
-const CounterContainer = ({ number, increase, decrease }) => {
+const CounterContainer = () => {
+  // useSelector Hook을 사용하면 connect 함수를 사용하지 않고도 리덕스의 상태를 조회할 수 있다.
+  // 여기서 상태 선택 함수는 mapStateToProps와 형태가 똑같다.
+  const number = useSelector((state) => state.counter.number);
+
+  // 컨테이너 컴포넌트에서 액션을 디스패치해야 한다면 이 Hook을 사용하면 된다.
+  const dispatch = useDispatch();
+
+  // useCallback()을 사용하는 이유는
+  // 숫자가 바뀌어서 컴포넌트가 리렌더링될 때마다 onIncrease 함수와 onDecrease 함수가 새롭게 만들어진다.
+  // 성능 최적화를 위해 useCallback으로 디스패치를 감싸주면 불필요한 렌더링을 막을 수 있다.
+  const onIncrease = useCallback(() => dispatch(increase()), [dispatch]);
+  const onDecrease = useCallback(() => dispatch(decrease()), [dispatch]);
+
   return (
-    <Counter number={number} onIncrease={increase} onDecrease={decrease} />
+    <Counter number={number} onIncrease={onIncrease} onDecrease={onDecrease} />
   );
 };
-/*
-// mapDispatchToProps, mapDispatchToProps에서 반환하는 객체 내부의 값들은 컴포넌트의 props로 전달된다.
-// mapStateToProps, 현재 스토어가 지니고 있는 상태를 가리킴
-const mapStateToProps = (state) => ({
-  number: state.counter.number,
-});
 
-// mapDispatchToProps, store의 내장 함수 dispatch를 파라미터로 받아 사용
-const mapDispatchToProps = (dispatch) => ({
-  increase: () => {
-    dispatch(increase());
-  },
-  decrease: () => {
-    dispatch(decrease());
-  },
-});
-*/
-
-/* 
-// connect를 통해서 해당 컴포넌트를 연결시키고,
-// mapStateToProps, mapDispatchToProps안에 있는 값들을 해당 컴포넌트의 props로 전달
-// 전달된 number, increase, decrease를 사용
-const mapDispatchToProps = (dispatch) => ({
-  increase: () => {
-    console.log("increase");
-  },
-  decrease: () => {
-    console.log("decrease");
-  },
-});
-*/
-// 액션 생성 함수의 개수가 많아질 경우, 리덕스에서 제공하는 bindAcrionCreators 유틸 함수를 사용
-/* 
-이전 예시
-export default connect(
-  (state) => ({
-    number: state.counter.number,
-  }),
-  (dispatch) => ({
-    increase: () => dispatch(increase()),
-    decrease: () => dispatch(decrease()),
-  })
-)(CounterContainer);
-
-bindActionCreators가 적용된 예시
-export default connect(
-  (state) => ({
-    number: state.counter.number,
-  }),
-  (dispatch) =>
-    bindActionCreators(
-      {
-        increase,
-        decrease,
-      },
-      dispatch
-    )
-)(CounterContainer);
-*/
-// bindActionCreators보다 더 편한 방법은 connect 함수가 내부적으로 bindActionCreators 작업을 대신해준다.
-export default connect(
-  (state) => ({
-    number: state.counter.number,
-  }),
-  {
-    increase,
-    decrease,
-  }
-)(CounterContainer);
+export default CounterContainer;
--- src/containers/TodosContainer.js
+++ src/containers/TodosContainer.js
@@ -1,39 +1,52 @@
-import React from "react";
-import { connect } from "react-redux";
+import React, { useCallback } from "react";
+import { useSelector, useDispatch } from "react-redux";
 import { changeInput, insert, toggle, remove } from "../modules/todos";
 import Todos from "../components/Todos";
+import { useActions } from "../lib/useActions";
 
-const TodosContainer = ({
-  input,
-  todos,
-  changeInput,
-  insert,
-  toggle,
-  remove,
-}) => {
+const TodosContainer = () => {
+  const { input, todos } = useSelector(({ todos }) => ({
+    input: todos.input,
+    todos: todos.todos,
+  }));
+
+  // const dispatch = useDispatch();
+  // const onChangeInput = useCallback((input) => dispatch(changeInput(input)), [
+  //   dispatch,
+  // ]);
+  // const onInsert = useCallback((text) => dispatch(insert(text)), [dispatch]);
+  // const onToggle = useCallback((id) => dispatch(toggle(id)), [dispatch]);
+  // const onRemove = useCallback((id) => dispatch(remove(id)), [dispatch]);
+
+  // useActions Hook은 액션 생성 함수를 액션을 디스패치하는 함수로 자동으로 변환해 준다.
+  // 첫번째 파라미터에는 액션 생성 함수들로 이루어진 배열
+  // 두번째 파라미터에는 첫번째 파라미터에 있는 원소가 바뀌면 액션을 디스패치하는 함수를 새로 만듬
+  const [onChangeInput, onInsert, onToggle, onRemove] = useActions(
+    [changeInput, insert, toggle, remove],
+    []
+  );
+
   return (
     <Todos
       input={input}
       todos={todos}
-      onChangeInput={changeInput}
-      onInsert={insert}
-      onToggle={toggle}
-      onRemove={remove}
+      onChangeInput={onChangeInput}
+      onInsert={onInsert}
+      onToggle={onToggle}
+      onRemove={onRemove}
     />
   );
 };
 
-export default connect(
-  // 비구조화 할당을 통해 todos를 분리
-  // state.todos.input 대신 todos.input을 사용
-  ({ todos }) => ({
-    input: todos.input,
-    todos: todos.todos,
-  }),
-  {
-    changeInput,
-    insert,
-    toggle,
-    remove,
-  }
-)(TodosContainer);
+/* 
+connect 함수를 사용하여 컨테이너 컴포넌트를 만들었을 경우, 해당 컨테이너 컴포넌트의 부모 컴포넌트가 리렌더링될 때 
+해당 props가 바뀌지 않았다면 리렌더링이 자동으로 방지되어 성능이 최적화된다.
+
+반면에, connect 함수 대신 useSelector를 사용하여 리덕스 상태를 조회했을 경우
+자동으로 최적화 작업이 이루어지지 않기 때문에, 성능 최적화를 위해서는 
+React,memo를 컨테이너에 사용해주어야 한다.
+
+*/
+export default React.memo(TodosContainer);
+
+// 물론 지금과 같은 경우에는 TodosContainer의 부모 컴포넌트인 App 컴포넌트가 리렌더링되는 일이 없으므로 불필요한 성능 최적화이다.
